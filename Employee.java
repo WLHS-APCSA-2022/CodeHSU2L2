373591846b137719f79cd43b0a2183a7637bbf12
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-25 13:00:01 -0700

    create template Employee.java

---
+++ Employee.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Employee here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Employee
+{
+    // instance variables - replace the example below with your own
+    //every employee has a name, employeeID, and pay
+
+    /**
+     * Constructor for objects of class Employee
+     * this is the default constructor
+     */
+    public Employee()
+    {
+        // initialise instance variables
+    
+
+    }
+
+    //this is overloading, use all parameters
+    public Employee(){
+        
+
+    }
+    
+    //another overload using some parameters
+    public Employee(){
+    
+    }
+
+}
